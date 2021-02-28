7e712eaecbebc7e3fdf0753faa65d4a5f1f8b1f1
Sunil Bagde <bagdesunil786@gmail.com>
2021-03-01 02:32:23 +0530

    doubly linked lsit

--- 2-DoublyLinkedList/2-DoublyLinkedList.js
+++ 2-DoublyLinkedList/2-DoublyLinkedList.js
@@ -35,4 +35,49 @@ class DoublyLinkedList {
             this.lastNode = newNode;
         }
     }
+    removeFromFront() {
+       if(!this.firstNode) {
+           return null;
+       } 
+       const removedNode = this.firstNode;
+       this.firstNode = this.firstNode.nextNode;
+       return removedNode;
+    }
 }
+
+
+class Queue {
+
+    constructor () {
+        this.queue = null;
+        this.data = new DoublyLinkedList();
+    }
+
+    enqueue(element) {
+        this.data.insertAtEnd(element);
+    }
+
+    dequeue() {
+        const removedNode = this.data.removeFromFront();
+        return removedNode;
+    }
+    read() {
+     if(this.data.firstNode) {
+         return this.data.firstNode.data
+     }    
+     return null   
+    }
+}
+
+
+const queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+ 
+console.log("queue", queue.dequeue());
+ console.log("queue", queue.read());
+ 
+
+
+
